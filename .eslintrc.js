fd35f6823447e0a4bf4b7eaa273bd0446499927a
Rumpel <hades137747575@gmail.com>
2021-01-05 16:27:26 +0800

    配置代码格式化

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: 'warn',
+    'no-console': 'off',
+    'no-debugger': 'off',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'spaced-comment': ['error', 'always'],
+    'no-trailing-spaces': ['error'],
+    'typescript/class-name-casing': 'off',
+    eqeqeq: [
+      'error',
+      'always',
+      {
+        null: 'ignore',
+      },
+    ],
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+};
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Step 1
+# 烹饪步骤
+
+## Step 1
 
 `npm init @vitejs/app`
+
+## Step 2
+
+`npm i -D eslint eslint-plugin-vue @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-typescript prettier @vitejs/plugin-vue-jsx`
