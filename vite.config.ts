a691ace82e7ca1f8de69c0963136d86f73d7cfc1
Rumpel <hades137747575@gmail.com>
2021-01-07 10:05:07 +0800

    vita 2.0 没有对jsx做特殊处理esbuild会默认以react的形式编译增加babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
-export default defineConfig({
-  plugins: [vue()],
+const vueConfig = defineConfig({
+  plugins: [vue(), vueJsx()],
 });
+
+const config = {
+  ...vueConfig,
+};
+
+export default config;
